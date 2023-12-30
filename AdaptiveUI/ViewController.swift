9c1e85db6fe2a16ada603938b0ed0a3900d7b5b6
Software <81501384+gali981101@users.noreply.github.com>
2023-12-30 23:13:25 +0800

    Add all files.

---
+++ AdaptiveUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdaptiveUI
+//
+//  Created by Terry Jason on 2023/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
